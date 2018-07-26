d945b2b1892a97a694cdbfdd94950fb77fec8bc3
hiroshishimada <shsm385@gmail.com>
2018-07-26 15:19:45 +0900

    数字に対応しました

--- app.js
+++ app.js
@@ -3,8 +3,14 @@
 const axios = require(`axios`);
 const APIKEY = `ee87e39793802c011bb0a765e847125d7c5fea1a3bfc981c76f53c7ef2e246fc`; //API KEY
 const BASE_URL = `https://labs.goo.ne.jp/api/hiragana`;
-let array = ['アップル','飲み会','一文字','パイナップル','みかん','田中'];
-const SENTENCE = array;
+let array = ['４５','アップル','飲み会','一文字','パイナップル','みかん','田中','２３','１３'];
+let nums = array.filter(function (element){
+  return element.match(/[０-９．]/g);
+});
+
+let sarr = diffArray(array,nums);
+
+const SENTENCE = sarr;
 //const SENTENCE = process.argv[2];
 const OUTPU_TYPE = `katakana`; //or `hiragana`
 
@@ -31,24 +37,38 @@ function katakanaToHiragana(src) {
     });
 }
 
+//Listの差分を求める
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  for(var a = 0 ; a < arr1.length; a++){
+    if(arr2.indexOf(arr1[a]) === -1 ){
+      newArr.push(arr1[a]);
+    }
+  }
+  for(var b = 0; b < arr2.length; b++){
+    if(arr1.indexOf(arr2[b]) === -1 ){
+       newArr.push(arr2[b]);
+       }
+  }
+  return newArr;
+}
 
 axios(options)
 .then((res) => {
     let str = res.data.converted;
     let arr = str.split(' ');
-    //let arr = Object.keys(res.data).map(function () {return obj});
-    console.log(
-      arr.sort(function(char1,char2){
-      char1 = katakanaToHiragana(char1.toString());
-      char2 = katakanaToHiragana(char2.toString());
-      if(char1 < char2){
-        return -1;
-      }else if(char1 > char2){
-        return 1;
-      }
-      return 0;
-      })
-    );
+    let result = nums.sort().concat(arr.sort(
+    function(char1,char2){
+    char1 = katakanaToHiragana(char1.toString());
+    char2 = katakanaToHiragana(char2.toString());
+    if(char1 < char2){
+      return -1;
+    }else if(char1 > char2){
+      return 1;
+    }
+    return 0;
+    }));
+    console.log(result);
 })
 .catch((err) => {
     console.log(err);
